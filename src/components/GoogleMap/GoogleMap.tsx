2012f0c00062d946ba7791425a999df3d5a2dc4d
Maksat <boktaban@gmail.com>
2023-09-25 17:59:38 +0600

    remove eslint errors

--- src/components/GoogleMap/GoogleMap.tsx
+++ src/components/GoogleMap/GoogleMap.tsx
@@ -1,27 +1,8 @@
 import GoogleMapReact from 'google-map-react'
 import { cn } from '@/lib/utils.ts'
 import { useLocalStorage } from '@/hooks/useLocalStorage.ts'
-import { useState } from 'react'
-
-export type MarkerProps = {
-  lat: number
-  lng: number
-  text: string
-}
-const Marker = (props: MarkerProps) => {
-  const { lat, lng, text } = props
-  return (
-    <div className={cn('text-white bg-red-500 rounded-full w-6 h-6 border-4')}>
-      {text}
-    </div>
-  )
-}
 
 export const GoogleMap = () => {
-  const [markerPosition, setMarkerPosition] = useState({
-    lat: 51.088749,
-    lng: 71.419069,
-  })
   const [locationStorage] = useLocalStorage('location', { lng: 42, lat: 60 })
 
   const defaultProps = {
@@ -32,19 +13,7 @@ export const GoogleMap = () => {
     zoom: 15,
   }
 
-  // const handleMapChange = (newMapProps: GoogleMapReact.ChangeEventValue) => {
-  //   console.log(markerPosition)
-  //   console.log(newMapProps.center)
-  //
-  //   // setMarkerPosition((prev) => ({
-  //   //   lat: prev.lat + newMapProps.marginBounds.nw.lat,
-  //   //   lng: prev.lng + newMapProps.marginBounds.nw.lng,
-  //   // }))
-  // }
-
-  const handleApiLoaded = ({ map, maps }) => {
-    // console.log(map)
-  }
+  const handleApiLoaded = () => {}
 
   return (
     <div className={cn('w-full h-full')}>
